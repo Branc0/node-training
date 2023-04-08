f9a16d3e9aa321e2e65e0dcefd876a2c923dfb91
Rafael Rocha de Azevedo <rrochadeazevedo@gmail.com>
2023-04-08 18:27:06 -0300

    feat: implements PUT, FIND, DELETE

--- src/controller/books.controller.js
+++ src/controller/books.controller.js
@@ -6,13 +6,43 @@ class BookController {
     res.status(200).json(booksCollection);
   };
 
+  static findBook = async (req, res) => {
+    const id = req.params.id;
+    const book = await books.findById(id);
+    if (!book) {
+      res.status(500).send({ message: `${book}` });
+    } else {
+      res.status(200).send(book);
+    }
+  };
+
   static addBook = async (req, res) => {
     const book = new books(req.body);
     const dbRes = await book.save();
     if (dbRes.errors) {
-      res.status(500).send({ message: `${err.message}` });
+      res.status(500).send({ message: `${dbRes.errors.message}` });
     } else {
       res.status(201).send(book.toJSON());
+    }
+  };
+
+  static updateBook = async (req, res) => {
+    const id = req.params.id;
+    const dbResult = await books.findByIdAndUpdate(id, { $set: req.body });
+    if (dbResult.errors) {
+      res.status(500).send({ message: `${dbRes.errors.message}` });
+    } else {
+      res.status(200).send({ message: "updated successfully" });
+    }
+  };
+
+  static deleteBook = async (req, res) => {
+    const id = req.params.id;
+    const dbResult = await books.findByIdAndDelete(id);
+    if (dbResult.errors) {
+      res.status(500).send({ message: `${dbRes.errors.message}` });
+    } else {
+      res.status(200).send({ message: "deleted successfully" });
     }
   };
 }
--- src/routes/books.routes.js
+++ src/routes/books.routes.js
@@ -4,6 +4,9 @@ import BookController from "../controller/books.controller.js";
 const router = express.Router();
 
 router.get("/books", BookController.getAllBooks);
+router.get("/books/:id", BookController.findBook);
 router.post("/books", BookController.addBook);
+router.put("/books/:id", BookController.updateBook);
+router.delete("/books/:id", BookController.deleteBook);
 
 export default router;
