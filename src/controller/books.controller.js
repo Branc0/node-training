8156925af53765e06af51b52071c9b3613b60a48
Rafael Rocha de Azevedo <rrochadeazevedo@gmail.com>
2023-04-08 16:59:30 -0300

    feat: implements POST w/ database

--- src/controller/books.controller.js
+++ src/controller/books.controller.js
@@ -5,6 +5,16 @@ class BookController {
     const booksCollection = await books.find();
     res.status(200).json(booksCollection);
   };
+
+  static addBook = async (req, res) => {
+    const book = new books(req.body);
+    const dbRes = await book.save();
+    if (dbRes.errors) {
+      res.status(500).send({ message: `${err.message}` });
+    } else {
+      res.status(201).send(book.toJSON());
+    }
+  };
 }
 
 export default BookController;
--- src/routes/books.routes.js
+++ src/routes/books.routes.js
@@ -4,5 +4,6 @@ import BookController from "../controller/books.controller.js";
 const router = express.Router();
 
 router.get("/books", BookController.getAllBooks);
+router.post("/books", BookController.addBook);
 
 export default router;
