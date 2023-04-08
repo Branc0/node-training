0d0781b8c6a338f22c05fae4fd76ba7c53a2c344
Rafael Rocha de Azevedo <rrochadeazevedo@gmail.com>
2023-04-08 12:53:17 -0300

    installing Express

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
-const http = require("http");
-const port = 3000;
+import app from "./src/app.js";
+const port = process.env.PORT || 3000;
 
-const rotas = {
-  "/": "Node Course",
-  "/books": "Books section",
-  "/authors": "Authors section",
-};
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end(rotas[req.url]);
-});
-
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server listening to port ${port}`);
 });
