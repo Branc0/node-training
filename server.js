d5f1240679c37fefb65b457a2276b464802d68aa
Rafael Rocha de Azevedo <rrochadeazevedo@gmail.com>
2023-04-08 12:42:02 -0300

    installing nodemon

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require("http");
 const port = 3000;
 
+const rotas = {
+  "/": "Node Course",
+  "/books": "Books section",
+  "/authors": "Authors section",
+};
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("My first node app");
+  res.end(rotas[req.url]);
 });
 
 server.listen(port, () => {
