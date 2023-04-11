09f323ae111921d46804df75fb077ef60bbd1352
Rafael Rocha de Azevedo <rrochadeazevedo@gmail.com>
2023-04-10 23:22:39 -0300

    feat: adds autopopulate

--- src/models/Book.js
+++ src/models/Book.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import autopopulate from 'mongoose-autopopulate';
 
 const bookSchema = new mongoose.Schema({
   id: {type: String},
@@ -7,11 +8,12 @@ const bookSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'authors',
     required: true,
+    autopopulate: true,
   },
   editor: {type: String, required: true},
   amountOfPages: {type: Number},
 });
-
+bookSchema.plugin(autopopulate);
 const books = mongoose.model('books', bookSchema);
 
 export default books;
